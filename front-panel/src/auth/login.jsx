49afce11e7291305f69c933f843e2a35ec448ecc
Ali Heider Swccho <ali.heider.swccho@gmail.com>
2024-01-17 22:34:10 +0600

    login page done

--- front-panel/src/auth/login.jsx
+++ front-panel/src/auth/login.jsx
@@ -1,31 +1,79 @@
-import React from "react";
+import React, {useState} from "react";
 
 // Icons
 import { RiAccountPinCircleLine } from "react-icons/ri";
 import { IoLockClosedOutline } from "react-icons/io5";
+import { setCookie } from '../services/cookies.jsx';
+
 
 // Link to replace anchor tag <a></a>
 import {Link} from "react-router-dom";
+import api from "../services/api.jsx";
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const [errors, setErrors] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+    const handleSignIn = async () => {
+        try {
+            setIsLoading(true);
+            const result = await api.post('/auth/login', formData);
+            if (result.access_token) {
+                setIsLoading(false);
+                setCookie('authToken', result.access_token, { expires: 7 });
+            } else {
+                setErrors(result);
+                setIsLoading(false);
+            }
+        } catch (error) {
+            setIsLoading(false);
+            console.error("Error during login:", error);
+        }
+    };
+
+    const renderError = (fieldName) => {
+        if (errors[fieldName]) {
+            return <div className="text-danger">{errors[fieldName]}</div>;
+        }
+        return null;
+    };
+
+
     return (
         <>
             <div className="sign-in-form">
                 <h2 className="form-title">Sign in</h2>
-                <form method="POST" className="register-form" id="login-form">
+                <form method="POST"  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSignIn();
+                }} className="register-form" id="login-form">
                     <div className="form-group form-theme has-icon-left mb-3">
-                        <label htmlFor={'your_name'} className="input-icon"><i ><RiAccountPinCircleLine /></i></label>
-                        <input type="text" name="your_name" id="your_name" className={'form-control'} placeholder="Your Name"/>
+                        <label htmlFor={'email'} className="input-icon"><i ><RiAccountPinCircleLine /></i></label>
+                        <input type="text" value={formData.email}
+                               onChange={handleInputChange} name="email" id="email" className={'form-control'} placeholder="Email Address"/>
+                        {renderError("email")}
                     </div>
                     <div className="form-group form-theme has-icon-left mb-2">
-                        <label htmlFor={'your_name'} className="input-icon"><i ><IoLockClosedOutline /></i></label>
-                        <input type="password" name="your_pass" className={'form-control'} id="your_pass" placeholder="Password"/>
+                        <label htmlFor={'password'} className="input-icon"><i ><IoLockClosedOutline /></i></label>
+                        <input type="password" value={formData.password} onChange={handleInputChange}  name="password" className={'form-control'} id="password" placeholder="Password"/>
+                        {renderError("password")}
                     </div>
                     <div className="form-group text-end mb-3">
                         <Link  to={'/forgot'}>Forgot Password</Link>
                     </div>
                     <div className="form-group text-start mb-3">
-                        <button type="button" name="signin" id="signin" className="btn btn-theme btn-auth w-100" >Sign In</button>
+                        <button type="submit" name="signin" id="signin" disabled={isLoading} className="btn btn-theme btn-auth w-100" >{isLoading ? 'Signing in...' : 'Sign In'}</button>
                     </div>
                     <div className="form-group text-center mb-3">
                         Do not have an account? <Link  to={'/register'}> Register Now</Link>
