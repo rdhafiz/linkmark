b2dcfc78d32f3d8954e19a3c503523d437db9b54
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2024-01-18 15:37:56 +0600

    dashboard UI has been completed

--- front-panel/src/layouts/includes/header.jsx
+++ front-panel/src/layouts/includes/header.jsx
@@ -6,34 +6,34 @@ function Header() {
     const [isDropdown, setIsDropdown] = useState(false);
     return (
         <>
-            <div className="header d-flex justify-content-between align-items-center">
-                <Link to={'/home'} className="brand-logo">Linkmark</Link>
+            <div className="header">
+                <div className="container d-flex justify-content-between align-items-center">
+                    <Link to={'/home'} className="brand-logo">Linkmark</Link>
 
-                <div data-bs-toggle="dropdown" aria-expanded="false" className="avatar"
-                     onClick={() => setIsDropdown(!isDropdown)}>
-                    <img
-                        src="https://ui-avatars.com/api/?background=6dabe4&color=fff&rounded=true&bold=true&name=R+C"
-                        alt="avatar"/>
+                    <div data-bs-toggle="dropdown" aria-expanded="false" className="avatar"
+                         onClick={() => setIsDropdown(!isDropdown)}>
+                        <img
+                            src="https://ui-avatars.com/api/?background=6dabe4&color=fff&rounded=true&bold=true&name=R+C"
+                            alt="avatar"/>
 
-                    {/*Header dropdown start*/}
-                    {isDropdown ? (<>
-                        <ul className="dropdown-menu show header-dropdown border-0 shadow rounded-3 p-3">
-                            <li className="d-flex align-items-center justify-content-between mb-4">
-                                <div className="name fs-5 fw-medium flex-grow-1">Rahat Chowdhury</div>
-                                <div className="drop-avatar">
-                                    <img
-                                        src="https://ui-avatars.com/api/?background=6dabe4&color=fff&rounded=true&bold=true&name=Rahat+Chowdhury"
-                                        alt="avatar"/>
-                                </div>
-                            </li>
-                            <li><Link to={'/profile'} className="dropdown-item rounded-3 mb-2">Profile</Link></li>
-                            <li><a className="dropdown-item rounded-3" href="#">Logout</a></li>
-                        </ul>
-                    </>) : <></>}
-                    {/*Header dropdown end  */}
+                        {/*Header dropdown start*/}
+                        {isDropdown ? (<>
+                            <ul className="dropdown-menu show header-dropdown border-0 shadow rounded-3 p-3">
+                                <li className="d-flex align-items-center justify-content-between mb-4">
+                                    <div className="name fs-5 fw-medium flex-grow-1">Rahat Chowdhury</div>
+                                    <div className="drop-avatar">
+                                        <img
+                                            src="https://ui-avatars.com/api/?background=6dabe4&color=fff&rounded=true&bold=true&name=Rahat+Chowdhury"
+                                            alt="avatar"/>
+                                    </div>
+                                </li>
+                                <li><Link to={'/profile'} className="dropdown-item rounded-3 mb-2">Profile</Link></li>
+                                <li><a className="dropdown-item rounded-3" href="#">Logout</a></li>
+                            </ul>
+                        </>) : <></>}
+                        {/*Header dropdown end  */}
+                    </div>
                 </div>
-
-
             </div>
         </>
     )
--- front-panel/src/pages/home.jsx
+++ front-panel/src/pages/home.jsx
@@ -1,8 +1,90 @@
 import React from "react";
+
+
+import '../stylesheets/pages/home.scss'
+
+// icons
+import {IoLink} from "react-icons/io5";
+import { FcFolder } from "react-icons/fc";
+
 function Home() {
     return (
         <>
-          Home
+            <div className="home container">
+                <div className="home-content p-3">
+                    <div className="link-box rounded-3 shadow-sm">
+                        <div
+                            className="link-box-cover rounded-top-3 d-flex justify-content-center align-items-center">
+                            <i><IoLink/></i>
+                        </div>
+                        <div className="link-box-content p-3 bg-dark rounded-bottom-3">
+                            <h5 className="title fw-bold">Title</h5>
+                            <p className="small desc mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="link">https://github.com</p>
+                        </div>
+                    </div>
+
+                    <div className="link-box rounded-3 shadow-sm">
+                        <div
+                            className="link-box-cover rounded-top-3 d-flex justify-content-center align-items-center">
+                            <i><IoLink/></i>
+                        </div>
+                        <div className="link-box-content p-3 bg-dark rounded-bottom-3">
+                            <h5 className="title fw-bold">Title</h5>
+                            <p className="small desc mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="link">https://github.com</p>
+                        </div>
+                    </div>
+
+                    <div className="link-box rounded-3 shadow-sm">
+                        <div
+                            className="link-box-cover rounded-top-3 d-flex justify-content-center align-items-center">
+                            <i><IoLink/></i>
+                        </div>
+                        <div className="link-box-content p-3 bg-dark rounded-bottom-3">
+                            <h5 className="title fw-bold">Title</h5>
+                            <p className="small desc mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="link">https://github.com</p>
+                        </div>
+                    </div>
+
+                    <div className="link-box rounded-3 shadow-sm">
+                        <div
+                            className="link-box-cover rounded-top-3 d-flex justify-content-center align-items-center">
+                            <i><IoLink/></i>
+                        </div>
+                        <div className="link-box-content p-3 bg-dark rounded-bottom-3">
+                            <h5 className="title fw-bold">Title</h5>
+                            <p className="small desc mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="link">https://github.com</p>
+                        </div>
+                    </div>
+
+                    <div className="link-box rounded-3 shadow-sm">
+                        <div
+                            className="link-box-cover folder rounded-top-3 d-flex justify-content-center align-items-center">
+                            <FcFolder />
+                        </div>
+                        <div className="link-box-content p-3 bg-dark rounded-bottom-3">
+                            <h5 className="title fw-bold">Title</h5>
+                            <p className="small desc mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="link">https://github.com</p>
+                        </div>
+                    </div>
+
+                    <div className="link-box rounded-3 shadow-sm">
+                        <div
+                            className="link-box-cover rounded-top-3 d-flex justify-content-center align-items-center">
+                            <i><IoLink/></i>
+                        </div>
+                        <div className="link-box-content p-3 bg-dark rounded-bottom-3">
+                            <h5 className="title fw-bold">Title</h5>
+                            <p className="small desc mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="link">https://github.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
