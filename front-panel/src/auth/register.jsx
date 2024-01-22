7a68fab8c76d8a34d3254e100aa56b350fbf07d4
Ali Heider Swccho <ali.heider.swccho@gmail.com>
2024-01-22 08:02:53 +0600

    fixing websiote

--- front-panel/src/auth/register.jsx
+++ front-panel/src/auth/register.jsx
@@ -1,35 +1,30 @@
-import React, {useState} from "react";
-
-// Icons
+import React, { useState } from "react";
 import { RiAccountPinCircleLine } from "react-icons/ri";
-import { IoLockClosedOutline } from "react-icons/io5";
-import { MdOutlineMarkEmailRead } from "react-icons/md";
-
-// Link to replace anchor tag <a></a>
-import {Link, Navigate, redirect, useNavigate} from "react-router-dom";
+import { IoLockClosedOutline, IoMailOutline } from "react-icons/io5";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../services/api.jsx";
-import {setCookie} from "../services/cookies.jsx";
+import { setCookie } from "../services/cookies.jsx";
+import { renderError } from "../services/RenderError.jsx";
+import { handleInputChange } from "../services/InputUtils.jsx";
 
 const Register = () => {
+    const navigate = useNavigate();
+
     const [formData, setFormData] = useState({
         name: "",
         email: "",
         password: "",
         password_confirmation: "",
     });
+
     const [errors, setErrors] = useState({});
     const [isLoading, setIsLoading] = useState(false);
 
-    const navigate = useNavigate()
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+    const onInputChange = (event) => {
+        handleInputChange(event, formData, setFormData);
     };
-    const handleSignUp = async () => {
+
+    const signUp = async () => {
         setErrors({});
         try {
             setIsLoading(true);
@@ -38,7 +33,7 @@ const Register = () => {
                 setIsLoading(false);
                 setCookie('authToken', result.access_token, { expires: 7 });
                 setCookie('userInfo', JSON.stringify(result.data), { expires: 7 });
-                navigate("/")
+                navigate("/");
             } else {
                 setErrors(result);
                 setIsLoading(false);
@@ -49,49 +44,91 @@ const Register = () => {
         }
     };
 
-    const renderError = (fieldName) => {
-        if (errors[fieldName]) {
-            return <div className="text-danger">{errors[fieldName]}</div>;
-        }
-        return null;
-    };
-
     return (
-        <>
-            <div className="sign-in-form">
-                <h2 className="form-title">Sign up</h2>
-                <form method="POST" className="register-form" id="login-form" onSubmit={(e) => {e.preventDefault();handleSignUp();}}>
-                    <div className="form-group form-theme has-icon-left mb-3">
-                        <label htmlFor={'name'} className="input-icon"><i ><RiAccountPinCircleLine /></i></label>
-                        <input type="text" name="name" id="name" className={'form-control'} placeholder="Name" value={formData.name} onChange={handleInputChange}/>
-                        {renderError("name")}
-                    </div>
-                    <div className="form-group form-theme has-icon-left mb-3">
-                        <label htmlFor={'email'} className="input-icon"><i ><MdOutlineMarkEmailRead /></i></label>
-                        <input type="text" name="email" id="email" className={'form-control'} placeholder="Email Address" value={formData.email} onChange={handleInputChange} />
-                        {renderError("email")}
-                    </div>
-                    <div className="form-group form-theme has-icon-left mb-2">
-                        <label htmlFor={'password'} className="input-icon"><i ><IoLockClosedOutline /></i></label>
-                        <input type="password" name="password" className={'form-control'} id="password" placeholder="Password" value={formData.password} onChange={handleInputChange}/>
-                        {renderError("password")}
-                    </div>
-                    <div className="form-group form-theme has-icon-left mb-2">
-                        <label htmlFor={'password_confirmation'} className="input-icon"><i ><IoLockClosedOutline /></i></label>
-                        <input type="password" name="password_confirmation" className={'form-control'} id="password_confirmation" placeholder="Repeat Password" value={formData.password_confirmation} onChange={handleInputChange}/>
-                        {renderError("password_confirmation")}
-                    </div>
-                    <br/>
-                    <div className="form-group text-start">
-                        <button type="submit" name="signin" id="signin" disabled={isLoading} className="btn btn-theme btn-auth w-100" >{isLoading ? 'Signing Up...' : 'Sign up'}</button>
-                    </div>
-                    <br/>
-                    <div className="form-group text-center mb-3">
-                        Already have an account? <Link  to={'/'}> Sign in</Link>
-                    </div>
-                </form>
-            </div>
-        </>
+        <div className="sign-in-form">
+            <h2 className="form-title">Sign up</h2>
+            <form onSubmit={(e) => { e.preventDefault(); signUp(); }} className="register-form" id="login-form">
+                {/* Name input */}
+                <div className="form-group form-theme has-icon-left mb-3">
+                    <label htmlFor="name" className="input-icon"><i><RiAccountPinCircleLine /></i></label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="form-control"
+                        placeholder="Name"
+                        value={formData.name}
+                        onChange={onInputChange}
+                        aria-label="Name"
+                    />
+                    {renderError("name", errors)}
+                </div>
+                {/* Email input */}
+                <div className="form-group form-theme has-icon-left mb-3">
+                    <label htmlFor="email" className="input-icon"><i><IoMailOutline /></i></label>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        className="form-control"
+                        placeholder="Email Address"
+                        value={formData.email}
+                        onChange={onInputChange}
+                        aria-label="Email Address"
+                    />
+                    {renderError("email", errors)}
+                </div>
+                {/* Password input */}
+                <div className="form-group form-theme has-icon-left mb-2">
+                    <label htmlFor="password" className="input-icon"><i><IoLockClosedOutline /></i></label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={onInputChange}
+                        aria-label="Password"
+                    />
+                    {renderError("password", errors)}
+                </div>
+                {/* Password confirmation input */}
+                <div className="form-group form-theme has-icon-left mb-2">
+                    <label htmlFor="password_confirmation" className="input-icon"><i><IoLockClosedOutline /></i></label>
+                    <input
+                        type="password"
+                        name="password_confirmation"
+                        className="form-control"
+                        id="password_confirmation"
+                        placeholder="Repeat Password"
+                        value={formData.password_confirmation}
+                        onChange={onInputChange}
+                        aria-label="Repeat Password"
+                    />
+                    {renderError("password_confirmation", errors)}
+                </div>
+                <br />
+                {/* Sign-up button */}
+                <div className="form-group text-start">
+                    <button
+                        type="submit"
+                        name="signin"
+                        id="signin"
+                        disabled={isLoading}
+                        className="btn btn-theme btn-auth w-100"
+                        aria-label={isLoading ? 'Signing Up...' : 'Sign Up'}
+                    >
+                        {isLoading ? 'Signing Up...' : 'Sign Up'}
+                    </button>
+                </div>
+                <br />
+                {/* Sign-in link */}
+                <div className="form-group text-center mb-3">
+                    Already have an account? <Link to={'/'}> Sign in</Link>
+                </div>
+            </form>
+        </div>
     );
 };
 
